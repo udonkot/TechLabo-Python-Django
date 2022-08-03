10431e99aed5872b2283f0bdb48b4b70db43e63f
udonkot <udonko104@gmail.com>
2022-08-03 23:55:13 +0900

    ソース整形、集計結果を表示するよう修正

--- apps/main.py
+++ apps/main.py
@@ -11,12 +11,15 @@ from msrest.authentication import CognitiveServicesCredentials
 from dotenv import load_dotenv
 load_dotenv()
 
+# 環境変数取得
 KEY = os.getenv('KEY')
 KEY2 = os.getenv('KEY2')
 ENDPOINT = os.getenv('ENDPOINT')
 
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
+"""
+FaceAPI サンプル実装
 # Detect a face in an image that contains a single face
 single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
 
@@ -35,25 +38,28 @@ print()
 
 # Save this ID for use in Find Similar
 first_image_face_ID = detected_faces[0].face_id
+"""
 
+# サンプル画像
 single_face_image_url = 'https://farm3.static.flickr.com/2496/3800942346_90c2ce1d54.jpg'
 #single_face_image_url = 'https://img.cinematoday.jp/a/RBj7QU9ad-uB/_size_640x/_v_1629858458/main.jpg'
 #single_face_image_url = 'https://app.box.com/s/1ago5ti67baf27qfwfzlhmcpgtvzrkl8'
 
 # Attributes you want returned with the API call, a list of FaceAttributeType enum (string format)
+# FaceAPIの返却する項目(全項目)
 face_attributes = ['age', 'gender', 'headPose', 'smile', 'facialHair', 'glasses', 'emotion', 'hair', 'makeup', 'occlusion', 'accessories', ]
 
-# -----
-# Detect a face in an image that contains a single face
-#single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-single_image_name = os.path.basename(single_face_image_url)
-# We use detection model 3 to get better performance.
-#detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
-
 def getFaceSample(filePath):
+    """ 顔情報取得
+    
+    画像から検出した顔情報を返却する。
+
+    """    
+
 #    detected_faces = face_client.face.detect_with_url(url=imgData, return_face_landmarks=True, return_face_attributes=face_attributes)
-    print('Path:')
-    print(filePath)
+    # アップロードファイル取得
+    # print('Path:')
+    # print(filePath)
     imgData = open(filePath, 'rb')
 
     retArray = []
@@ -64,22 +70,25 @@ def getFaceSample(filePath):
     # ファイルパスを指定する場合はこちら
     # detected_faces = face_client.face.detect_with_url(url=single_face_image_url, return_face_attributes=face_attributes)
     
+    # 画像から顔情報が検出できない場合は終了
     if not detected_faces:
         return retArray
 #        raise Exception('No face detected from image {}'.format(filePath))
-
     
     # 取得結果解析
     for face in detected_faces:
-        # ログ出力
-        showFaceLog(face)
+        # Debug用ログ出力
+        # showFaceLog(face)
         faceImgData = Image.open(filePath)
         drawing = ImageDraw.Draw(faceImgData)
+
+        # 検出した顔に枠線追加
         drawing.rectangle(getRectangle(face), outline='Red', width = 3)
         
+        # 枠線を追加した画像を返却するようセット
         output = io.BytesIO()
         image_type = imghdr.what(filePath)
-        print('imgType : ' + image_type)
+        # print('imgType : ' + image_type)
         faceImgData.save(output, format=image_type)
         wrkArray = [face, base64.b64encode(output.getvalue()).decode("ascii")]
         retArray.append(wrkArray)
@@ -89,15 +98,17 @@ def getFaceSample(filePath):
 #    for face in detected_faces:
 #      drawing.rectangle(getRectangle(face), outline='Red', width = 3)
 #    faceImgData.show()
-
-    print("len:" + str(len(retArray)))
-        
         
     return retArray
 #    return faceInfo
 
 # Convert width height to a point in a rectangle
 def getRectangle(faceDictionary):
+    """ 顔座標取得
+    
+    画像から検出した顔の座標を返却する。
+
+    """    
     rect = faceDictionary.face_rectangle
     left = rect.left
     top = rect.top
@@ -106,36 +117,15 @@ def getRectangle(faceDictionary):
 
     return ((left, top), (right, bottom))
 
-def drawFaceRectangles() :
-    # Download the image from the url
-    response = requests.get(single_face_image_url)
-    img = Image.open(io.BytesIO(response.content))
-
-    # For each face returned use the face rectangle and draw a red box.
-    print('Drawing rectangle around face... see popup for results.')
-    draw = ImageDraw.Draw(img)
-    for face in detected_faces:
-        draw.rectangle(getRectangle(face), outline='red')
-
-    # Display the image in the default image browser.
-    # img.show()
-    
-    return img
-
-def drawImg(imgData):
-    draw = ImageDraw.Draw(imgData)
-    for face in detected_faces:
-        draw.rectangle(getRectangle(face), outline='red')
-
-    # Display the image in the default image browser.
-    # img.show()
-    
-    return imgData
-    
-# Face Attribute出力
 def showFaceLog(face):
+    """ Face Attributeコンソール出力
+    
+    取得したFaceオブジェクトの情報をprintで出力する
+
+    """    
+
     print()
-    print('Detected face ID from', os.path.basename(single_face_image_url), ':')
+    # print('Detected face ID from', os.path.basename(single_face_image_url), ':')
     # ID of detected face
     print(face.face_id)
     # Show all facial attributes from the results
--- apps/views.py
+++ apps/views.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 from apps.models import Upload
 
-from apps.main import getFaceSample
+from apps.main import getFaceSample, showFaceLog
 
 from facescore.settings import MEDIA_ROOT
 
@@ -53,6 +53,8 @@ def facescore(request):
     
     ret = getFaceSample(fname)
     
+    sumary = calcSummaryFaceScore(ret);
+    
     if not ret :
       context = {
         'files': documents,
@@ -66,6 +68,7 @@ def facescore(request):
 #     'uploadfile': base64.b64encode(Upload.uploaderFile),
 #      'uploadfile': encodeData,
        'detected_faces': ret,
+       'summary': sumary,
 #      'img': drawFaceRectangles(),
       }
 
@@ -77,6 +80,73 @@ def facescore(request):
     }
        
   # Uncomment this to show the face rectangles.
-  # drawFaceRectangles()
 
   return render(request, 'apps/facescore.html', context)
+
+def calcSummaryFaceScore(detected_faces) :
+  smile = 0;
+  happiness = 0;
+  anger = 0;
+  sadness = 0;
+  surprise = 0;
+  fear = 0;
+  contempt = 0;
+  disgust = 0;
+  neutral = 0;
+  
+  for faceData in detected_faces :
+    # 笑顔度
+    smile += faceData[0].face_attributes.smile;
+    # 幸福度
+    happiness += faceData[0].face_attributes.emotion.happiness;
+    # 怒り度</th>
+    anger += faceData[0].face_attributes.emotion.anger;
+    # 悲しみ度</th>
+    sadness += faceData[0].face_attributes.emotion.sadness;
+    # 驚き度</th>
+    surprise += faceData[0].face_attributes.emotion.surprise;
+    # 恐怖度</th>
+    fear += faceData[0].face_attributes.emotion.fear;
+    # 軽蔑度</th>
+    contempt += faceData[0].face_attributes.emotion.contempt;
+    # 嫌悪度</th>
+    disgust += faceData[0].face_attributes.emotion.disgust;
+    # 中性度</th>
+    neutral += faceData[0].face_attributes.emotion.neutral;
+
+  count = len(detected_faces)
+  smileScore = smile * 100 / count;
+  happinessScore = happiness * 100 / count;
+  angerScore = anger * 100 / count;
+  sadnessScore = sadness * 100 / count;
+  surpriseScore = surprise * 100 / count;
+  fearScore = fear * 100 / count;
+  contemptScore = contempt * 100 / count;
+  disgustScore = disgust * 100 / count;
+  neutralScore = neutral * 100 / count;
+  
+  positiveScore = round((happinessScore * 10) + (neutralScore * 5));
+  negativeScore = round((angerScore * 2) + (sadnessScore * 2) + (fearScore * 2) + (contempt * 5) + (disgustScore * 10)) ;
+  
+  TotalScore = positiveScore - negativeScore;
+
+   
+  ret = {
+    'smile': smile,
+    'happiness': happiness,
+    'anger': anger,
+    'sadness': sadness,
+    'surprise': surprise,
+    'fear': fear,
+    'contempt': contempt,
+    'disgust': disgust,
+    'neutral': neutral,
+    'positive': positiveScore,
+    'negative': negativeScore,
+    'total': TotalScore,
+  } 
+  
+  return ret
+    
+  
+  
