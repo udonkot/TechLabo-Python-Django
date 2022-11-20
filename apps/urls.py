5ca315b622be2b98e7c3ee5cb545e42ef498cb37
s-okayasu <40541121+s-okayasu@users.noreply.github.com>
2022-11-20 19:05:55 +0900

    パッケージ構成の追加＋不要ファイルの削除

--- apps/urls.py
+++ apps/urls.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+# Add your urls here.
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('facescore', views.facescore, name='facescore'),    
+    path('sample', views.sample, name='sample'),
+    path('okayasu', views.okayasu, name='okayasu'),
 ]
 
 if settings.DEBUG: 
