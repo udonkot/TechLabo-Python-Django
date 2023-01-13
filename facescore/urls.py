adfd5563d05d4c7cf74844b2e7c1e4680a4c038e
udonkot <udonko104develop@gmail.com>
2023-01-14 00:46:15 +0900

    [update]フォルダ構成変更

--- facescore/urls.py
+++ facescore/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    path('', apps.urls),
     path('apps/', include('apps.urls')),
     path('admin/', admin.site.urls),
-    
+
 ]
