325e18a90d850d0e329405e22342e468edc732e7
s-okayasu <40541121+s-okayasu@users.noreply.github.com>
2023-07-25 17:45:07 +0900

    不要ファイルの削除

--- apps/postgres_webapp/windows/database/SQLite.sql
+++ apps/postgres_webapp/windows/database/SQLite.sql
@@ -1,11 +1,14 @@
--- SQLite
-CREATE TABLE sample_table (sample_data VARCHAR(20));
+-- table
+CREATE TABLE database_sapmletable (sample_data VARCHAR(20));
 
-DROP TABLE sample_table;
+DROP TABLE database_sapmletable;
 
+-- recode
 INSERT INTO database_sapmletable (id, sample_data) VALUES (1, 'data-1'), (2, 'data-2');
 
 DELETE FROM database_sapmletable; 
---WHERE sample_data = 'sample data';
+--WHERE id = X;
 
 SELECT * FROM database_sapmletable;
+
+UPDATE database_sapmletable SET sample_data = 'data-X' WHERE id = X; 
