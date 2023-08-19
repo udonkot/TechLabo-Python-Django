3d2909d846a1dcab5870e41e6e8f6ab500fe4fd9
s-okayasu <40541121+s-okayasu@users.noreply.github.com>
2023-08-20 05:40:53 +0900

    urls.py変更

--- apps/urls.py
+++ apps/urls.py
@@ -5,18 +5,27 @@ from . import views
 
 # Add your urls here.
 urlpatterns = [
-    # 勉強会で使用するurls
+    # アプリ用のurls
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('sample', views.sample, name='sample'),
-    path('okayasu', views.okayasu, name='okayasu'),
-    path('hazeyama', views.hazeyama, name='hazeyama'),
-    path('kondo/', include('apps.kondo.urls'), name='kondoDir'),
     path('slackapi/', include('apps.slackapi.urls'), name='slackapiDir'),
     path('azure/', include('apps.azure.urls'), name='azureDir'),
+    path('index', views.index, name='index'),
 
-    # 勉強会で使用しないurls
-    path('index', views.index, name='index')
+    # ユーザ用のURL
+    # apps/{ユーザ名}配下のurls.pyへリクエストを転送する
+    #path('irito/', include('apps.irito.urls')),
+    path('okayasu/', include('apps.okayasu.urls')),
+    #path('kondo/', include('apps.kondo.urls')),
+    #path('kita/', include('apps.kita.urls')),
+    #path('hazama/', include('apps.hazama.urls')),
+    #path('hazeyama/', include('apps.hazeyama.urls')),
+    #path('matsuura/', include('apps.matsuura.urls')),
+    #path('yamane/', include('apps.yamane.urls')),
+
+    #path('hazeyama/', views.hazeyama, name='hazeyama'),
+    path('kondo/', include('apps.kondo.urls'), name='kondoDir'),
 ]
 
 if settings.DEBUG:
