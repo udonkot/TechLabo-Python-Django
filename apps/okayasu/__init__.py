e792fef023acb43a5a36b1b493293ef78606ee02
s-okayasu <40541121+s-okayasu@users.noreply.github.com>
2023-08-20 06:41:01 +0900

    __init__.py変更

--- apps/okayasu/__init__.py
+++ apps/okayasu/__init__.py
@@ -6,6 +6,6 @@ from apps.python_library.config import init
 
 # config情報を環境変数に設定
 base_dir = Path(__file__).resolve().parent.parent
-user_dir = os.environ['USER']
+user_dir = os.environ['APP']
 config_dir = os.path.join(base_dir,  user_dir, 'config', 'config.ini')
 init.set_environ(config_dir)
