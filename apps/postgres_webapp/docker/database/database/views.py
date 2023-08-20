59200aaa0a53d12e433ef259c3b7b22d4d646ca4
s-okayasu <40541121+s-okayasu@users.noreply.github.com>
2023-08-21 03:50:45 +0900

    修正前のバックアップファイルの削除

--- apps/postgres_webapp/docker/database/database/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from .modules import query
-
-
-def data(request):
-    result = query.select_data()
-
-    return HttpResponse(result)
--- apps/postgres_webapp/windows/database/database/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class SapmleTable(models.Model):
-    sample_data = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.sample_data
